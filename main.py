462755bca74dd4794597f12ba450949dc749f24f
parker davis <theparkerevan>
2025-02-25 15:26:56 -0500

    I GOT IT WORKING

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from rustat import test_class, test_func
+import time
+from rustat import test_class
 
-print(test_func(1_000_000_000))
+#print(test_func(1_000_000_000))
 
 temp = test_class(1987)
+
+amount = 1_000_000_000
+step = amount / 20
+next_step = step
+
+temp.run(amount)
+
+percent_done = 0
+while percent_done < 100:
+    if temp.get_count() >= next_step:
+        next_step += step
+        percent_done += 5
+        print(f"{percent_done}% - {temp.get_count()}")
+    time.sleep(.017)
--- rustat/src/lib.rs
+++ rustat/src/lib.rs
@@ -1,43 +1,62 @@
 use pyo3::prelude::*;
-
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
 
 #[pyclass]
 struct test_class {
-    inner: i32,
+    count: Arc<Mutex<u64>>,
 }
 
 #[pymethods]
 impl test_class {
     #[new]
-    fn new(value: i32) -> Self {
+    fn new(value: u64) -> Self {
         println!("Made with {}", value);
-        test_class { inner: value, }
+        test_class { count: Arc::new(Mutex::new(0u64)), }
+    }
+
+    fn run(&self, value: u64) {
+        println!("Hi!");
+        
+        let the_ref = self.count.clone();
+        thread::spawn(move || {
+            for _ in 0..value {
+                let mut data = the_ref.lock().unwrap();
+                *data += 1u64;
+            }
+            println!("done!");
+        });
+    }
+
+    fn get_count(&self) -> u64{
+        *self.count.lock().unwrap()
     }
 }
 
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn test_func(py: Python<'_>, a: u64) -> PyResult<u64> {
-    let count = py.allow_threads(|| count_to(a));
-    println!("AHHHHHHHHHHH");
+// /// Formats the sum of two numbers as string.
+// #[pyfunction]
+// fn test_func(py: Python<'_>, a: u64) -> PyResult<u64> {
+//     let count = py.allow_threads(|| count_to(a));
+//     println!("AHHHHHHHHHHH");
     
-    Ok(count)
-}
+//     Ok(count)
+// }
 
-fn count_to (a: u64) -> u64 {
-    let mut count = 0u64;
-    for _ in (0..a) {
-        count += 1u64;
-    }
+// fn count_to (a: u64) -> u64 {
+//     let mut count = 0u64;
+//     for _ in (0..a) {
+//         count += 1u64;
+//     }
     
-    count
-}
+//     count
+// }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rustat(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(test_func, m)?)?;
+    //m.add_function(wrap_pyfunction!(test_func, m)?)?;
     m.add_class::<test_class>()?;
     Ok(())
 }
